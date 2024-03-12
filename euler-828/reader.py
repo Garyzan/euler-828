61b7422b09c2fc01671b779ea95c8024dc087498
Garyzan <timo.meegdenburg@gmail.com>
2024-03-12 17:17:22 +0100

    finalize reader.py

--- euler-828/reader.py
+++ euler-828/reader.py
@@ -1,20 +1,17 @@
 import os
+
 from problem import Problem
 
-class Reader:
-    def __init__(self) -> None:
-        pass
+def parse_problem(problem: str) -> Problem:
+    target, ops = problem.split(":")
+    target = int(target)
+    ops = [int(k) for k in ops.split(",")]
+    return Problem(target, ops)
 
-    def parse_problem(self, problem: str) -> Problem:
-        target, ops = problem.split(":")
-        target = int(target)
-        ops = [int(k) for k in ops.split(",")]
-        return Problem(target, ops)
-
-    def read_file(self, path : os.PathLike | str) -> list[Problem]:
-        with open(path, "r") as f:
-            lines = f.readlines()
-            problems : list[Problem] = []
-            for line in lines:
-                problems.append(self.parse_problem(line.rstrip()))
-        return problems
+def read_file(path : os.PathLike | str) -> list[Problem]:
+    with open(path, "r") as f:
+        lines = f.readlines()
+        problems : list[Problem] = []
+        for line in lines:
+            problems.append(parse_problem(line.rstrip()))
+    return problems
