5ef870aa6f813d46fb53613b7273deee842c3b15
Garyzan <timo.meegdenburg@gmail.com>
2024-03-13 14:40:05 +0100

    create basic structure of solver

--- euler-828/solver.py
+++ euler-828/solver.py
@@ -1,7 +1,19 @@
+import seaborn
+import operator as o
 
 from problem import Problem
 from expression import Expression
+from typing import Union, Callable
 
 class Solver:
-    def __init__(self) -> None:
+    operators : list[Callable]
+
+    def __init__(self, operators : list[Callable] = [o.add, o.sub, o.mul, o.truediv]) -> None:
+        assert all([isinstance(operator, Callable) for operator in operators])
+        self.operators = operators
+
+    def solve(self, problem : Problem) -> Union[Expression, list[Expression]]:
+        pass
+
+    def plot_all_solutions(self, problem : Problem) -> None:
         pass
