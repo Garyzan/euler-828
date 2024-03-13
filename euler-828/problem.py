2fccef6d5135642b7f9180ec4e1001c74ca392d9
Garyzan <timo.meegdenburg@gmail.com>
2024-03-13 14:39:01 +0100

    remove operators from problem

--- euler-828/problem.py
+++ euler-828/problem.py
@@ -1,19 +1,13 @@
 import operator as o
 
 from numbers import Integral
-from typing import Callable
 
 class Problem:
     target : int = 0
     operands : list[int] = []
-    operators: list[Callable]
 
-    def __init__(self, target : Integral = 0,
-                 operands : list = [],
-                 operators : list[Callable] = [o.add, o.mul, o.truediv, o.sub]) -> None:
+    def __init__(self, target : Integral = 0, operands : list = []) -> None:
         assert type(target) == Integral
         assert all([type(operand) == Integral for operand in operands])
-        assert all([type(operator) == Callable for operator in operators])
         self.target = target
         self.operands = operands
-        self.operators = operators
