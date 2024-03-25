90d75608bc6451c3c23281b4e113be22c5c299fa
Garyzan <timo.meegdenburg@gmail.com>
2024-03-25 13:39:22 +0100

    added print functionality

--- euler-828/problem.py
+++ euler-828/problem.py
@@ -3,11 +3,14 @@ import operator as o
 from numbers import Integral
 
 class Problem:
-    target : int = 0
-    operands : list[int] = []
+    target : Integral = 0
+    operands : list[Integral] = []
 
     def __init__(self, target : Integral = 0, operands : list = []) -> None:
         assert isinstance(target, Integral)
         assert all([isinstance(operand, Integral) for operand in operands])
         self.target = target
         self.operands = operands
+
+    def __str__(self) -> str:
+        return f"{self.target} : " + ", ".join([str(k) for k in self.operands])
