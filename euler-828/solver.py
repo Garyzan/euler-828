ef560b1ed4414498c599dd3df0a27cdd98bf05e9
Garyzan <timo.meegdenburg@gmail.com>
2024-03-26 16:11:29 +0100

    dissolve Solver class

--- euler-828/solver.py
+++ euler-828/solver.py
@@ -16,6 +16,7 @@ class op:
         self.associative = associative
         self.risky = risky
 
+OPERATORS : list[op] = [op(o.add, True), op(o.sub, False), op(o.mul, True), op(o.truediv, False, True)]
 
 # checks if l2 is sublist of l1
 def sublist(l1, l2):
@@ -24,36 +25,44 @@ def sublist(l1, l2):
             return False
     return True
 
-class Solver:
-    operators : list[op] = [op(o.add, True), op(o.sub, False), op(o.mul, True), op(o.truediv, False, True)]
+def _build_expressions(operands : list[Integral], size : int) -> list[list[Expression]]:
+    if size == 1:
+        return [[Expression([k,], Node(k)) for k in operands],]
+    
+    sub_expressions = _build_expressions(operands, size-1)
+    sub_expressions.append([])
 
-    def __init__(self) -> None:
-        pass
+    for i in range(1, floor(size/2)+1):
+        for (idx, i_tree) in enumerate(sub_expressions[i-1]):
 
-    def build_expressions(self, operands : list[Integral], size : int) -> list[list[Expression]]:
-        if size == 1:
-            return [[Expression([k,], Node(k)) for k in operands],]
-        
-        sub_expressions = self.build_expressions(operands, size-1)
-        sub_expressions.append([])
+            start_idx = (0, idx+1)[i == size-i]
 
-        for i in range(1, floor(size/2)+1):
-            for (idx, i_tree) in enumerate(sub_expressions[i-1]):
+            for j_tree in sub_expressions[(size-i)-1][start_idx:]:
 
-                start_idx = (0, idx+1)[i == size-i]
+                if j_tree == i_tree:
+                    continue
 
-                for j_tree in sub_expressions[(size-i)-1][start_idx:]:
+                for operator in OPERATORS:
 
-                    if j_tree == i_tree:
+                    new_ops = j_tree.operands + i_tree.operands
+                    if not sublist(operands, new_ops):
                         continue
 
-                    for operator in self.operators:
+                    expr = Expression(new_ops, Node(operator.fun, i_tree, j_tree))
 
-                        new_ops = j_tree.operands + i_tree.operands
-                        if not sublist(operands, new_ops):
+                    if operator.risky:
+                        try:
+                            expr.evaluate()
+                        except FloatingPointError:
+                            continue
+                        except ZeroDivisionError:
                             continue
 
-                        expr = Expression(new_ops, Node(operator.fun, i_tree, j_tree))
+                    sub_expressions[size-1].append(expr)
+
+                    #if the operator is not associative, create new expression with swapped operands
+                    if not operator.associative:
+                        expr = Expression(new_ops, Node(operator.fun, j_tree, i_tree))
 
                         if operator.risky:
                             try:
@@ -62,48 +71,34 @@ class Solver:
                                 continue
                             except ZeroDivisionError:
                                 continue
-
                         sub_expressions[size-1].append(expr)
-
-                        #if the operator is not associative, create new expression with swapped operands
-                        if not operator.associative:
-                            expr = Expression(new_ops, Node(operator.fun, j_tree, i_tree))
-
-                            if operator.risky:
-                                try:
-                                    expr.evaluate()
-                                except FloatingPointError:
-                                    continue
-                                except ZeroDivisionError:
-                                    continue
-                            sub_expressions[size-1].append(expr)
-        return sub_expressions
+    return sub_expressions
 
 
-    def solve(self, problem : Problem) -> tuple[Integral, list[Expression]]:
+def solve(problem : Problem) -> tuple[Integral, list[Expression]]:
 
-        final_expr = []
-        min_score = sum(problem.operands) + 1
-        expressions = self.build_expressions(problem.operands, len(problem.operands))
+    final_expr = []
+    min_score = sum(problem.operands) + 1
+    expressions = _build_expressions(problem.operands, len(problem.operands))
 
-        for sublist in expressions:
-            for expr in sublist:
-                if expr.score > min_score:
-                    continue
+    for sublist in expressions:
+        for expr in sublist:
+            if expr.score > min_score:
+                continue
 
-                if expr.evaluate() != problem.target:
-                    continue
+            if expr.evaluate() != problem.target:
+                continue
 
-                if expr.score < min_score:
-                    min_score = expr.score
-                    final_expr = [expr]
-                    
-                else: final_expr.append(expr)
-        
-        if min_score > sum(problem.operands):
-            return 0, []
+            if expr.score < min_score:
+                min_score = expr.score
+                final_expr = [expr]
 
-        return min_score, final_expr
+            else: final_expr.append(expr)
+    
+    if min_score > sum(problem.operands):
+        return 0, []
+
+    return min_score, final_expr
 
 
     def plot_all_solutions(self, problem : Problem) -> None:
